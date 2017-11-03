0ef9b78d3183066dca2e5d08d8a96d7ccb9bfc81
archistico <info@archistico.com>
2017-11-03 18:52:56 +0100

    ok calcolo linee con commenti

--- clsoc/Contatore.cs
+++ clsoc/Contatore.cs
@@ -1,59 +1,105 @@
-﻿using System;
-using System.IO;
+﻿	using System;
+	using System.IO;
 
-namespace clsoc
-{
-	public class Contatore
+	namespace clsoc
 	{
-		public int lTxt;
-		public int lTxtEmpty;
-		public int lTxtComment;
-		public int nbTxt;
-
-		public Contatore()
+		public class Contatore
 		{
-			this.lTxt = 0;
-			this.lTxtEmpty = 0;
-			this.lTxtComment = 0;
-			this.nbTxt = 0;
-		}
+			public int linesOfCode;
+			public int linesOfcodeEmpty;
+			public int linesOfcodeComment;
+			public int linesOfcodeTotal;
+			public int numberOfFile;
 
-		public void Conteggia()
-		{
-			Console.WriteLine("CALCOLO NUMERO DI LINEE");
-			// HTML, PHP, JS, C#
-			this.ConteggiaPhp();
-			this.MostraRisultati();
-		}
-
-		public void ConteggiaPhp()
-		{
-			string[] filesTxt = Directory.GetFiles(Environment.CurrentDirectory, "*.txt", SearchOption.AllDirectories);
-			this.nbTxt = filesTxt.Length;
-
-			foreach (var file in filesTxt)
+			public Contatore()
 			{
-				string[] lines = System.IO.File.ReadAllLines(file);
-				for (int i = 0; i < lines.Length; i++)
+				this.linesOfCode = 0;
+				this.linesOfcodeEmpty = 0;
+				this.linesOfcodeComment = 0;
+				this.linesOfcodeTotal = 0;
+				this.numberOfFile = 0;
+			}
+
+			public void Conteggia(string ext)
+			{
+				Console.WriteLine("CALCOLO NUMERO DI LINEE");
+				// PHP, JS, C#
+				this.ConteggiaLinee(ext);
+				this.MostraRisultati();
+			}
+
+			public void ConteggiaLinee(string ext)
+			{
+				string[] filesTxt = Directory.GetFiles(Environment.CurrentDirectory, "*."+ext, SearchOption.AllDirectories);
+				this.numberOfFile = filesTxt.Length;
+
+				foreach (var file in filesTxt)
 				{
-					string line = lines[i];
-					if (line == string.Empty)
+					string[] lines = System.IO.File.ReadAllLines(file);
+					this.linesOfcodeTotal += lines.Length;
+					bool commento = false;
+
+					for (int i = 0; i < lines.Length; i++)
 					{
-						this.lTxtEmpty++;
-					}
-					else {
-						this.lTxt++;
+						string line = lines[i];
+						line = line.Trim();
+
+						if (line == string.Empty)
+						{
+							this.linesOfcodeEmpty++;
+							//Console.WriteLine(line + " -> Linea vuota");
+						}
+						else {
+						// Verifica se la linea è vuota o è un commento
+						if (commento == false && !line.StartsWith("/*") && !line.StartsWith("//"))
+						{
+							// Se non è all'interno di un commento e non inizia per ..
+							this.linesOfCode++;
+							//Console.WriteLine(line + " -> Linea codice");
+						}
+						else if (commento == false && line.StartsWith("//"))
+						{
+							// E' un commento singolo
+							commento = false;
+							this.linesOfcodeComment++;
+							//Console.WriteLine(line + " -> Commento singolo");
+						}
+						else {
+							// Tre casi ora
+							// 1 continua un commento
+							// 2 finisce un commento
+							// 3 inizia un commento
+							if (commento == true && !line.EndsWith("*/"))
+							{
+								this.linesOfcodeComment++;
+								//Console.WriteLine(line + " -> Commento continua");
+							}
+							else if (commento == true && line.EndsWith("*/"))
+							{
+								commento = false;
+								this.linesOfcodeComment++;
+								//Console.WriteLine(line + " -> Commento finisce");
+							}
+							else if (commento == false && line.StartsWith("/*")) {
+								commento = true;
+								this.linesOfcodeComment++;
+								//Console.WriteLine(line + " -> Commento inizia");
+							}
+
+						}
+						}
 					}
 				}
 			}
-		}
 
-		public void MostraRisultati()
-		{
-			Console.WriteLine("TXT numero file   : " + this.nbTxt.ToString());
-			Console.WriteLine("TXT linee         : " + this.lTxt.ToString());
-			Console.WriteLine("TXT linee vuote   : " +this.lTxtEmpty.ToString());
-			Console.WriteLine("TXT linee commenti: " + this.lTxtComment.ToString());
-		}
-}
-}
+			public void MostraRisultati()
+			{
+				Console.WriteLine("Numero file     : " + this.numberOfFile.ToString());
+				Console.WriteLine("Linee totali    : " + this.linesOfcodeTotal.ToString());
+				Console.WriteLine("-----------------------");
+				Console.WriteLine("Linee di codice : " + this.linesOfCode.ToString());
+				Console.WriteLine("Linee vuote     : " + this.linesOfcodeEmpty.ToString());
+				Console.WriteLine("Linee commenti  : " + this.linesOfcodeComment.ToString());
+			}
+	}
+	}
--- clsoc/Program.cs
+++ clsoc/Program.cs
@@ -6,8 +6,15 @@ namespace clsoc
 	{
 		public static void Main(string[] args)
 		{
+			if (args.Length < 1)
+			{
+				System.Console.WriteLine("Inserire l'estensione dei file da analizzare");
+				System.Console.WriteLine("Esempio: ./clsoc.exe txt");
+				Environment.Exit(1);
+			}
+
 			Contatore cont = new Contatore();
-			cont.Conteggia();
+			cont.Conteggia((string)args[0]);
 		}
 	}
 }
