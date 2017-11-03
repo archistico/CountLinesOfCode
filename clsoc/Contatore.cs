4a832fb6db435fe951be4a3ba614cc7ec5426772
archistico <info@archistico.com>
2017-11-03 17:29:37 +0100

    inizio

---
+++ clsoc/Contatore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace clsoc
+{
+	public class Contatore
+	{
+		public Contatore()
+		{
+		}
+
+		public void Conteggia()
+		{
+			// HTML, PHP, JS, C#
+			string[] filesTxt = Directory.GetFiles(@"\", "*.txt", SearchOption.AllDirectories);
+			foreach (var file in filesTxt)
+			{
+				Console.WriteLine(file);
+			}
+		}
+	}
+}
